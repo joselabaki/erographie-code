e1fd7f493cf1f2b986ac0d26a75ee9dfc02fadfa
joselabaki <jose.labaki@hotmail.com>
2018-12-30 12:52:42 +0100

    FAQ Phone

--- app/assets/javascripts/about.js
+++ app/assets/javascripts/about.js
@@ -15,5 +15,19 @@ $('#c-arrow').click(function() {
   $('.faq-c').slideDown('slow')
 })
 
+$('#close-faq').click(function() {
+  $(this).hide();
+  $('#c-arrow').show();
+  $('.faq-c-phone').slideUp('slow');
+})
+
+
+
+$('#c-arrow').click(function() {
+  $(this).hide();
+  $('#close-faq').show();
+
+  $('.faq-c-phone').slideDown('slow')
+})
 
 });
