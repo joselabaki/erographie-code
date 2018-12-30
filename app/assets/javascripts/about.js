8d04c02d423bf409aa291fbb06ab857105eb7c9e
joselabaki <jose.labaki@hotmail.com>
2018-12-30 21:21:17 +0100

    Modify phone

--- app/assets/javascripts/about.js
+++ app/assets/javascripts/about.js
@@ -1,5 +1,5 @@
 $(document).on('turbolinks:load', function() {
-  
+
 $('#close-faq').click(function() {
   $(this).hide();
   $('#c-arrow').show();
@@ -11,23 +11,23 @@ $('#close-faq').click(function() {
 $('#c-arrow').click(function() {
   $(this).hide();
   $('#close-faq').show();
-
   $('.faq-c').slideDown('slow')
 })
 
 $('#close-faq').click(function() {
+  $('.faq-c-phone').slideUp('slow');
   $(this).hide();
   $('#c-arrow').show();
-  $('.faq-c-phone').slideUp('slow');
 })
 
 
 
 $('#c-arrow').click(function() {
+  $('.faq-c-phone').slideDown('slow')
+
   $(this).hide();
   $('#close-faq').show();
 
-  $('.faq-c-phone').slideDown('slow')
 })
 
 });
